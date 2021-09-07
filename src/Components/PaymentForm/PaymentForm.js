fd9b3a175397a21745a74cf667596acbdedcba7b
ElementalEquityCode <me@danielvalencia.dev>
2021-09-06 21:56:35 -0400

    Added the InputAdornment to the card textfield.

--- src/Components/PaymentForm/PaymentForm.js
+++ src/Components/PaymentForm/PaymentForm.js
@@ -1,5 +1,7 @@
 import React from 'react';
-import { TextField } from '@material-ui/core';
+import { TextField, Button } from '@material-ui/core';
+import CreditCardIcon from '@material-ui/icons/Subtitles';
+import InputAdornment from '@material-ui/core/InputAdornment';
 import SectionLabel from '../SectionLabel/SectionLabel';
 import Grid from '../Grid/Grid';
 import styles from './PaymentForm.module.css';
@@ -15,35 +17,60 @@ class PaymentForm extends React.Component {
 
   render() {
     return (
-      <div className={styles.paymentForm}>
-        <SectionLabel>Client Information</SectionLabel>
-        <Grid
-          columns="one"
-        >
-          <TextField
-            label="Name on Card"
-            fullWidth
-          />
-          <TextField
-            label="Email"
-            fullWidth
-          />
-        </Grid>
-        <SectionLabel>Card Information</SectionLabel>
-        <Grid
-          columns="three"
-        >
-          <TextField
-            label="Card Number"
-          />
-          <TextField
-            label="Expiry"
-          />
-          <TextField
-            label="CVV"
-          />
-        </Grid>
-      </div>
+      <Grid
+        columns="one"
+        className={styles.overallGrid}
+      >
+        <div className={styles.paymentForm}>
+          <SectionLabel>Client Information</SectionLabel>
+          <Grid
+            columns="one"
+          >
+            <TextField
+              label="Name on Card"
+              fullWidth
+            />
+            <TextField
+              label="Email"
+              fullWidth
+            />
+          </Grid>
+          <SectionLabel>Card Information</SectionLabel>
+          <Grid
+            columns="three"
+          >
+            <TextField
+              label="Card Number"
+              InputProps={{
+                startAdornment: <InputAdornment position="start"><CreditCardIcon /></InputAdornment>
+              }}
+            />
+            <TextField
+              label="Expiry"
+            />
+            <TextField
+              label="CVV"
+            />
+          </Grid>
+          <div className={styles.buttonContainer}>
+            <Button
+              variant="contained"
+              className={styles.payButton}
+              sx={{
+                width: 100,
+              }}
+              style={{
+                color: 'white',
+                textTransform: 'none',
+                fontWeight: 500,
+                backgroundColor: 'rgb(67, 52, 189)'
+              }}
+            >
+              Pay $0.00
+            </Button>
+          </div>
+        </div>
+      </Grid>
     );
   }
 }
